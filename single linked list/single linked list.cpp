8e8b6fb6570b903cb45625d883c79cafebd19109
fasyaaa <muhammad.a.f.2210@gmail.com>
2023-05-19 15:08:06 +0700

    18

--- single linked list/single linked list.cpp
+++ single linked list/single linked list.cpp
@@ -32,4 +32,20 @@ void addNode() {
 	}
 
 	Node* previous = START;
-	START = nodeBaru;
+	Node* current = START;
+
+	while ((current != NULL) && (nim >= current->noMhs)) {
+		if (nim == current->noMhs) {
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+		previous = current;
+		current = current->next;
+	}
+	nodeBaru->next = current;
+	previous->next = nodeBaru;
+}
+
+bool searchNode(int nim, Node* current, Node* previous) {
+
+}
